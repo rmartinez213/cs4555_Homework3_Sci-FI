55b5154ab63b34d74fcde8f2f252f4999e5b3e74
rmartinez213 <rmartinez213@yahoo.com>
2018-12-09 14:48:33 -0800

    Sound complete

---
+++ cs4555_Homework3_Sci-FI-rob_branchV1/Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour {
+
+    public static AudioSource myAudio;
+   
+
+    void Awake() {
+        myAudio = GetComponent<AudioSource>();
+        myAudio.clip = Resources.Load<AudioClip>("Background_horror");
+        myAudio.ignoreListenerVolume = true;
+        myAudio.ignoreListenerPause = true;
+        myAudio.volume = 1;
+    }
+
+    public static void PlayMusic(){
+        myAudio.Play();
+    }
+
+    public static void StopMusic()
+    {
+        myAudio.clip.UnloadAudioData();
+        myAudio.Stop();
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ cs4555_Homework3_Sci-FI-rob_branchV1/Assets/Scripts/PlayerSoundsScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSoundsScript : MonoBehaviour {
+
+    //Sound Source
+    [SerializeField] private AudioSource soundSource; //player object
+    [SerializeField] private AudioClip jetpackSound; //audio clip
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Debug.Log("Vertical Velocity is: " + FPSInput.verticalVelocity);
+
+        if(FPSInput.verticalVelocity > 5 && !soundSource.isPlaying){
+            soundSource.PlayOneShot(jetpackSound);
+          //  Debug.Log("Play Sound Plz");
+        }
+
+        else if(FPSInput.verticalVelocity < 5 && soundSource.isPlaying){
+            soundSource.Stop();
+        }
+	}
+}
